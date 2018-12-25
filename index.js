37bc055a0887199b3292f0c7a10b878b65af1a78
FilippKramer <krame490@gmail.com>
2018-12-25 13:21:35 -0600

    Install nodemon and setup express server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 var app = require("express")();
+var http = require("http").Server(app);
+
+app.get('/', (req, res) => {
+  return res.send('<h1>Hello Wordl</h1>');
+});
+
+
+http.listen(3000, () => console.log("Listening on 3000"));
