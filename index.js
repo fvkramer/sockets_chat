3334ebc6b603a5b889423fc4c721752e7360265c
FilippKramer <krame490@gmail.com>
2018-12-26 17:00:33 -0600

    Initial work on html5 socket game

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+// var app = require("express")();
+// var http = require("http").Server(app);
+// var io = require("socket.io")(http);
+
+// app.get('/', (req, res) => {
+//   return res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function (socket) {
+//   console.log(`${socket.id}`);
+//   socket.on('chat message', function (msg) {
+//     io.emit('chat message', msg);
+//   });
+// });
+
+// http.listen(3000, () => console.log("Listening on 3000"));
+
+
+const app = require("express")();
+const httpServer = require('http').Server(app)
+const io = require("socket.io")(httpServer);
 
 app.get('/', (req, res) => {
   return res.sendFile(__dirname + '/index.html');
-});
+})
 
-io.on('connection', function (socket) {
-  console.log(`${socket.id}`);
-  socket.on('chat message', function (msg) {
-    io.emit('chat message', msg);
-  });
-});
+io.on('connection', socket => {
+  console.log("Socket connected");
+})
 
-http.listen(3000, () => console.log("Listening on 3000"));
+httpServer.listen(3000, () => console.log("Listening on server 3000"));
